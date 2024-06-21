fae936369c7face17e14a1ec8e1af6eb8417cc5e
redcolorbicycle <liuzhengyang10@gmail.com>
2024-06-21 16:10:30 +0800

    Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+
+  }
+
+  render () {
+    return (
+      <h1>APP</h1>
+    );
+  }
+
+}
+
+export default App;
