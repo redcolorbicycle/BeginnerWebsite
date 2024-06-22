cac07702006a6242149d0ba315dae6dc748fbd02
redcolorbicycle <liuzhengyang10@gmail.com>
2024-06-22 15:44:05 +0800

    Add background to webpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   state = {
+    advice: ''
+  };
 
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('http://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+        console.log(advice);
+      })
+      .catch((error) => {
+        console.log(error);
+
+      });
+    
   }
 
   render () {
+    const { advice } = this.state;
     return (
-      <h1>APP</h1>
+      <div className = "app">
+        <div className="card">
+          <h1>{advice}</h1>
+        </div>
+      </div>
     );
   }
 
